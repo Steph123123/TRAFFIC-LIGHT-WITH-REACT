3ead0f1eb9372b565a0a8f8325afe105a180abae
Steph123123 <steph13am@gmail.com>
2022-03-10 15:20:29 +0000

    lijtoooooo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const selectedcolor
+	const [color, setColor] = useState(null);
 	return (
 		<div className="containercito">
 			<div className="header">
@@ -13,12 +13,27 @@ const Home = () => {
 			</div>
 			<div className="contcenter">
 				<div className="traffic">
-					<div class="container">
-						<div className="red brillo"></div>
-						<div className="yellow"></div>
-						<div className="green"></div>
+					<div className="container">
+						<div
+							className={`red ${
+								color === "red" ? "light" : null
+							}`}
+							onClick={() => setColor("red")}></div>
+						<div
+							className={`yellow ${
+								color === "yellow" ? "light" : null
+							}`}
+							onClick={() => setColor("yellow")}></div>
+						<div
+							className={`green ${
+								color === "green" ? "light" : null
+							}`}
+							onClick={() => setColor("green")}></div>
 					</div>
 				</div>
+				<img
+					src="https://i.pinimg.com/originals/49/51/86/495186babd73e488363da30a986a53a3.jpg"
+					alt=""></img>
 			</div>
 		</div>
 	);
